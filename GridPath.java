8ee211936390d0e32e4ec67c78a1ac378d712a2f
Asadulnyc <asadulk@nycstudents.net>
2024-10-21 15:50:52 +0000

    first commit

---
+++ GridPath.java
@@ -0,0 +1,12 @@
+public GridPath(int[][] values)
+{
+    grid = values;
+
+    public Location getNextloc(int row, int col) {
+        return null;
+    }
+    public int sumpath(int row, int col)
+    {
+        return 0;
+    }
+}
